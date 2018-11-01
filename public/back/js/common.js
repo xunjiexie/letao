992e56868e0db7282f6b5d2c609b5ee2b66b7dcd
xunjiexie <1318252884@qq.com>
2018-11-01 08:26:03 +0800

    bug修复

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,15 @@
+$(document).ajaxStart(function() {
+  // 第一个 ajax 开始发送时调用, 开启进度条
+  NProgress.start();
+});
+$(document).ajaxStop(function() {
+  // 在所有的 ajax 请求完成时调用
+  // 模拟网络环境, 添加延迟
+  setTimeout(function() {
+    NProgress.done();
+  }, 500 );
+});
+
 $(function(){
   $.ajax({
     type:'get',
--- public/back/js/user.js
+++ public/back/js/user.js
@@ -8,7 +8,7 @@ $(function(){
       pageSize:5
     },
     success:function(info){
-      console.log(info);
+      // console.log(info);
       var htmlStr = template('tmp', info);
       // console.log(htmlStr);
       $('tbody').html(htmlStr);
